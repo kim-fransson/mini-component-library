f42175b58fa1f7ab1e204e4cbd4e354c9e83d0a9
Kim Fransson <45737053+kim-fransson@users.noreply.github.com>
2025-06-27 15:04:10 +0200

    Progressbar (#1)
    progressbar done

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,78 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const STYLES = {
+  small: {
+    height: 8,
+    radius: 4,
+    padding: 0,
+  },
+  medium: {
+    height: 12,
+    radius: 4,
+    padding: 0,
+  },
+  large: {
+    height: 16,
+    radius: 8,
+    padding: 4,
+  },
 };
+
+const ProgressBar = ({ value, label, size }) => {
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
+  return (
+    <Wrapper
+      role='progressbar'
+      aria-valuenow={value}
+      aria-valuemin={0}
+      aria-valuemax={100}
+      style={{
+        "--padding": styles.padding + "px",
+        "--radius": styles.radius + "px",
+      }}
+    >
+      <VisuallyHidden>${value}%</VisuallyHidden>
+      <StatusWrapper style={{ "--radius": styles.radius / 2 + "px" }}>
+        <StatusTrack
+          style={{
+            "--width": value + "%",
+            "--height": styles.height + "px",
+            "--radius": styles.radius / 2 + "px",
+          }}
+        />
+      </StatusWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background: ${COLORS.transparentGray15};
+  border-radius: var(--radius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+`;
+
+const StatusWrapper = styled.div`
+  border-radius: var(--radius);
+  /* Trim off corners when track is close to the right edge */
+  overflow: hidden;
+`;
+
+const StatusTrack = styled.div`
+  background: ${COLORS.primary};
+  border-radius: var(--radius) 0 0 var(--radius);
+  width: var(--width);
+  height: var(--height);
+`;
 
 export default ProgressBar;
