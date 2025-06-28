c7feec5ff119595d4d3d59344be66a3e6e7aba9d
Kim Fransson <45737053+kim-fransson@users.noreply.github.com>
2025-06-28 22:11:16 +0200

    select (#2)

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,64 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
-const Select = ({ label, value, onChange, children }) => {
+const Select = ({
+  label,
+  value,
+  onChange,
+  children,
+  ...delegated
+}) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <HiddenSelect value={value} onChange={onChange} {...delegated}>
+        {children}
+      </HiddenSelect>
+
+      <VisibleSelect>
+        {displayedValue}
+        <Icon id='chevron-down' strokeWidth={2} size={24} />
+      </VisibleSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+
+const HiddenSelect = styled.select`
+  position: absolute;
+  inset: 0;
+  background: transparent;
+  border: none;
+  outline: none;
+  color: transparent;
+  appearance: none;
+`;
+
+const VisibleSelect = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  border-radius: 8px;
+  background: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  color: ${COLORS.gray700};
+
+  ${HiddenSelect}:focus + & {
+    outline: 2px solid ${COLORS.primary};
+  }
+
+  ${HiddenSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
